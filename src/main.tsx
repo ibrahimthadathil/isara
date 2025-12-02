0cf61f82eeeb360d7e1c08aa88468f2da922e94c
ibrahim <ibrahiminfo8@gmail.com>
2025-12-02 23:33:22 +0530

    fest: created folder structure and routing

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./assets/index.css";
 import "../public/assets/custom.css";
 import "../public/assets/main.css";
+import { RouterProvider } from "react-router-dom";
+import  router  from "./router/router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    
+        <RouterProvider router={router} />
   </StrictMode>
 );
---
+++ src/router/router.tsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../components/layout/Index";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Terms from "../pages/Terms";
+
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/terms",
+        element: <Terms />,
+      },
+    ],
+  },
+]);
+
+export default router;
